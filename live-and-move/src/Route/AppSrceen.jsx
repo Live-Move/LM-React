ccfe112728074c72d0eaee1eda065db2299a91e3
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-09-11 10:06:13 +0900

    fit: [Login & Route] add test page

--- live-and-move/src/Route/AppSrceen.jsx
+++ live-and-move/src/Route/AppSrceen.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import useWindowSize from "../hooks/useWindowSize";
-import styled from "styled-components";
 import AppPages from "./AppPages";
 import NavForRoute from "./NavForRoute";
 
 function AppSrceen(props) {
-  const windowSize = useWindowSize();
   return (
     <>
       <NavForRoute />
