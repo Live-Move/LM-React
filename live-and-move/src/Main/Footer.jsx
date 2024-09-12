295e2e997e6623bef5ed5cfc34a1a95d9efaa9e9
wijihooooon <dnlwlgns1117@naver.com>
2024-09-12 13:56:23 +0900

    feat:[footer] optimization footer

--- live-and-move/src/Main/Footer.jsx
+++ live-and-move/src/Main/Footer.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import {
   FooterDiv,
-  FooterLeft,
-  FooterTop,
-  FooterTopMenuUl,
-  FooterTopMenuLi,
-  TopATagColorChange,
-  FooterBottomMenu,
-  FooterBottomMenuUl,
-  FooterBottomMenuLi,
-  BottomATagColorChange,
-  FooterBottomTelUl,
-  FooterBottomTelLi,
-  FooterBottomInfoUl,
-  FooterBottomInfoSpan,
-  FooterBottomCopyRight,
+  Container,
+  Top,
+  Ul,
+  TopMenuLi,
+  A,
+  BottomMenu,
+  BottomMenuLi,
+  BottomTelLi,
+  BottomInfoSpan,
+  BottomCopyRight,
   FooterRight,
-  SocialMediaATag,
+  SocialMediaLogo,
+  SocialMediaA,
   SocialMediaH3,
   SocialMediaSpan,
 } from "./FooterCss.js";
@@ -109,113 +106,144 @@ function Footer(props) {
 
   return (
     <FooterDiv>
-      <FooterLeft>
-        <FooterTop>
-          <div className="footer-top-menu">
-            <FooterTopMenuUl>
-              {FooterTopMenuList.map(({ title, path }, index) => {
-                return (
-                  <FooterTopMenuLi key={title + index}>
-                    <TopATagColorChange href={path}>{title}</TopATagColorChange>
-                  </FooterTopMenuLi>
-                );
-              })}
-            </FooterTopMenuUl>
+      <Container>
+        <div className="Left">
+          <Top>
+            <div className="top-menu">
+              <Ul>
+                {FooterTopMenuList.map(({ title, path }, index) => {
+                  return (
+                    <TopMenuLi key={title + index}>
+                      <A href={path} textColor={"white"}>
+                        {title}
+                      </A>
+                    </TopMenuLi>
+                  );
+                })}
+              </Ul>
+            </div>
+          </Top>
+
+          <div className="Bottom">
+            <BottomMenu>
+              <Ul>
+                {FooterBottomMenuList.map(({ title, path }, index) => {
+                  return (
+                    <BottomMenuLi key={title + index}>
+                      <A href={path} textColor={"#8D8D8D"}>
+                        {title}
+                      </A>
+                    </BottomMenuLi>
+                  );
+                })}
+              </Ul>
+            </BottomMenu>
+
+            <div className="Bottom-Tel">
+              <Ul>
+                <BottomTelLi>T. 제휴/대리점 개설문의 051-8711-3309</BottomTelLi>
+                <BottomTelLi>M. liveandmove@gmaiil.com</BottomTelLi>
+              </Ul>
+            </div>
+
+            <div className="Bottom-Info">
+              <Ul>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#BDBABD"}
+                    className="footer-bottom-info-title"
+                  >
+                    (주)리브앤무브
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#BDBABD"}
+                    className="footer-bottom-info-title"
+                  >
+                    대표이사
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#878687"}
+                    className="footer-bottom-info-content"
+                  >
+                    조동희 이은석 위지훈
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#BDBABD"}
+                    className="footer-bottom-info-title"
+                  >
+                    본사
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#878687"}
+                    className="footer-bottom-info-content"
+                  >
+                    부산광역시 동래구 낙민동 온천천로 339번길
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#BDBABD"}
+                    className="footer-bottom-info-title"
+                  >
+                    개인정보관리 책임자
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#878687"}
+                    className="footer-bottom-info-content"
+                  >
+                    고옴치
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#BDBABD"}
+                    className="footer-bottom-info-title"
+                  >
+                    사업자등록번호
+                  </BottomInfoSpan>
+                </li>
+                <li>
+                  <BottomInfoSpan
+                    textColor={"#878687 "}
+                    className="footer-bottom-info-content"
+                  >
+                    001-741861-01-014
+                  </BottomInfoSpan>
+                </li>
+              </Ul>
+            </div>
+
+            <BottomCopyRight>
+              Copyrightⓒ Live And Move Corporation. All Rights Reserved.
+            </BottomCopyRight>
           </div>
-        </FooterTop>
-
-        <div className="footer-bottom">
-          <FooterBottomMenu>
-            <FooterBottomMenuUl>
-              {FooterBottomMenuList.map(({ title, path }, index) => {
-                return (
-                  <FooterBottomMenuLi key={title + index}>
-                    <BottomATagColorChange href={path}>
-                      {title}
-                    </BottomATagColorChange>
-                  </FooterBottomMenuLi>
-                );
-              })}
-            </FooterBottomMenuUl>
-          </FooterBottomMenu>
-
-          <div className="footer-bottom-tel">
-            <FooterBottomTelUl>
-              <FooterBottomTelLi>
-                T. 제휴/대리점 개설문의 051-8711-3309
-              </FooterBottomTelLi>
-              <FooterBottomTelLi>M. liveandmove@gmaiil.com</FooterBottomTelLi>
-            </FooterBottomTelUl>
-          </div>
-
-          <div className="footer-bottom-info">
-            <FooterBottomInfoUl>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-title">
-                  (주)리브앤무브
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-title">
-                  대표이사
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-content">
-                  조동희 이은석 위지훈
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-title">
-                  본사
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-content">
-                  부산광역시 동래구 낙민동 온천천로 339번길
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-title">
-                  개인정보관리 책임자
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-content">
-                  고옴치
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-title">
-                  사업자등록번호
-                </FooterBottomInfoSpan>
-              </li>
-              <li>
-                <FooterBottomInfoSpan className="footer-bottom-info-content">
-                  001-741861-01-014
-                </FooterBottomInfoSpan>
-              </li>
-            </FooterBottomInfoUl>
-          </div>
-
-          <FooterBottomCopyRight>
-            Copyrightⓒ Live And Move Corporation. All Rights Reserved.
-          </FooterBottomCopyRight>
         </div>
-      </FooterLeft>
-      <FooterRight>
-        <div>
-          {FooterBottomSocialMediaList.map((logo, index) => (
-            <SocialMediaATag key={index} href="logo.href">
-              <img src={logo.src} alt="logo"></img>
-            </SocialMediaATag>
-          ))}
+        <FooterRight>
           <div>
-            <SocialMediaH3>고객센터 8711-3309</SocialMediaH3>
-            <SocialMediaSpan>(유료)</SocialMediaSpan>
+            <SocialMediaLogo>
+              {FooterBottomSocialMediaList.map((logo, index) => (
+                <SocialMediaA key={index} href="logo.href">
+                  <img src={logo.src} alt="logo"></img>
+                </SocialMediaA>
+              ))}
+            </SocialMediaLogo>
+            <div>
+              <SocialMediaH3>고객센터 8711-3309</SocialMediaH3>
+              <SocialMediaSpan>(유료)</SocialMediaSpan>
+            </div>
           </div>
-        </div>
-      </FooterRight>
+        </FooterRight>
+      </Container>
     </FooterDiv>
   );
 }
