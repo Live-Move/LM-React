8b692d5cf0183518428189ca6b856070d3a0dba3
wijihooooon <dnlwlgns1117@naver.com>
2024-09-14 04:38:48 +0900

    feat:[Main] create MainPageCarouselCss.js, fix Carousel & Img List

---
+++ live-and-move/src/Main/MainPageCarouselCss.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+/////////////////////////////////////////////////////////////////////
+export const CaptionContainer = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding-left: 20px;
+  text-align: left;
+  color: black;
+  margin-left: 214px;
+`;
+export const TextContainer = styled.div`
+  width: 570px;
+  height: 150px;
+  margin-bottom: 350px;
+  margin-right: 100px;
+`;
+
+export const H1 = styled.h1`
+  weight: 700;
+  font-size: 50px;
+  color: black;
+`;
+export const Span = styled.span`
+  weight: 700;
+  font-size: 20px;
+  color: black;
+`;
--- live-and-move/src/Main/MainPageCss.js
+++ live-and-move/src/Main/MainPageCss.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     margin 0px;
     padding 0px;
+
 `;
 export const CarouselContainer = styled.div`
   margin-bottom: 70px;
