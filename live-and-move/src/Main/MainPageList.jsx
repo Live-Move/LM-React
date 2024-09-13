1d2159168138bf2dc19b7f91825a342050b89bcf
wijihooooon <dnlwlgns1117@naver.com>
2024-09-13 11:59:44 +0900

    feat:[Main] MainPage Carousel & Image List

---
+++ live-and-move/src/Main/MainPageList.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Container, Img, Li, Span, Ul } from "./MainPageListCss";
+
+const PhotoList = [
+  {
+    src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/1.png?raw=true",
+    path: "/",
+    category: "테이블",
+  },
+  {
+    src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/2.png?raw=true",
+    path: "/",
+    category: "의자",
+  },
+  {
+    src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/3.png?raw=true",
+    path: "/",
+    category: "소파",
+  },
+  {
+    src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/4.png?raw=true",
+    path: "/",
+    category: "옷장",
+  },
+  {
+    src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/5.png?raw=true",
+    path: "/",
+    category: "인테리어 소품",
+  },
+];
+
+function MainPageList(props) {
+  return (
+    <Container>
+      <Ul>
+        {PhotoList.map(({ src, path, category }, index) => {
+          return (
+            <Li key={src + index}>
+              <a href={path}>
+                <Img src={src} alt={category} />
+                <Span>{category}</Span>
+              </a>
+            </Li>
+          );
+        })}
+      </Ul>
+    </Container>
+  );
+}
+
+export default MainPageList;
