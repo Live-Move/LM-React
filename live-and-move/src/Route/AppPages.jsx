d250c59c460f3d9c4d960765018ff29eb51977c5
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-09-13 15:58:56 +0900

    fix: [Route] remove ViewArea.jsx

--- live-and-move/src/Route/AppPages.jsx
+++ live-and-move/src/Route/AppPages.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppRouteDef } from "./RouteDef";
-import ViewArea from "./Responsive/ViewArea";
 
 function AppPages(props) {
   return (
-    <div style={{ height: "100vh" }}>
-      <BrowserRouter>
-        <ViewArea>
-          <Routes>
-            {Object.entries({ ...AppRouteDef }).map(
-              ([name, { path, element }], index) => (
-                <Route key={name + index} path={path} element={element} />
-              )
-            )}
-          </Routes>
-        </ViewArea>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {Object.entries({ ...AppRouteDef }).map(
+          ([name, { path, element }], index) => (
+            <Route key={name + index} path={path} element={element} />
+          )
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
