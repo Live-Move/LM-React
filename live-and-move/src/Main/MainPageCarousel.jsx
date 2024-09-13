58ce27108aea446b6cb95fb21869668c290e3269
wijihooooon <dnlwlgns1117@naver.com>
2024-09-13 16:20:14 +0900

    feat:[Main] Add title&content on Carousel, fix Img List

--- live-and-move/src/Main/MainPageCarousel.jsx
+++ live-and-move/src/Main/MainPageCarousel.jsx
@@ -4,12 +4,18 @@ import Carousel from "react-bootstrap/Carousel";
 const PhotoList = [
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/MainCarousel/1.png?raw=true",
+    title: "리브앤무브",
+    content: "합리적인 가격으로 만나는 프리미엄 가구",
   },
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/MainCarousel/2.png?raw=true",
+    title: "리브앤 무브2",
+    content: "리브앤 무브2",
   },
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/MainCarousel/3.png?raw=true",
+    title: "리브앤 무브3",
+    content: "리브앤 무브3",
   },
 ];
 
@@ -23,6 +29,13 @@ function MainPageCarousel(props) {
             alt={`Slide ${index + 1}`}
             style={{ width: "100%", height: "700px", objectFit: "cover" }} // 이미지의 높이와 너비 설정
           />
+
+          <Carousel.Caption>
+            <div>
+              <h3>{photo.title}</h3>
+              <p>{photo.content}</p>
+            </div>
+          </Carousel.Caption>
         </Carousel.Item>
       ))}
     </Carousel>
