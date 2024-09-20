d816b60aa6f6b5ad3b67153c78f2ee3b5bf3d4ee
wijihooooon <dnlwlgns1117@naver.com>
2024-09-20 11:19:03 +0900

    feat:[Route] Delete NavForRoute on AppScreen.jsx

--- live-and-move/src/Main/MainPage.jsx
+++ live-and-move/src/Main/MainPage.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import MainPageCarousel from "./MainPageCarousel";
 import MainPageList from "./MainPageList";
 import { CarouselContainer, Container } from "./MainPageCss";
-import Header from "./Header";
 
 function MainPage(props) {
   return (
     <Container>
-      <Header />
       <CarouselContainer>
         <MainPageCarousel />
       </CarouselContainer>
