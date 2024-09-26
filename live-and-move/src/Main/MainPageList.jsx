fd31e443469018018525d9f19b9864660bcf71be
wijihooooon <dnlwlgns1117@naver.com>
2024-09-27 03:18:30 +0900

    feat:[Main] update MainPageList path

--- live-and-move/src/Main/MainPageList.jsx
+++ live-and-move/src/Main/MainPageList.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container, Img, Li, Span, Ul } from "./MainPageListCss";
+import { useNavigate } from "react-router-dom";
 
 const PhotoList = [
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/1.png?raw=true",
-    path: "/",
     category1: "테이블",
     category2: "",
     value1: "50%",
@@ -12,7 +12,6 @@ const PhotoList = [
   },
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/2.png?raw=true",
-    path: "/",
     category1: "의자",
     category2: "",
     value1: "50%",
@@ -20,15 +19,13 @@ const PhotoList = [
   },
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/3.png?raw=true",
-    path: "/",
-    category1: "소파",
+    category1: "침대",
     category2: "",
     value1: "50%",
     value2: "50%",
   },
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/4.png?raw=true",
-    path: "/",
     category1: "옷장",
     category2: "",
     value1: "50%",
@@ -36,7 +33,6 @@ const PhotoList = [
   },
   {
     src: "https://github.com/Live-Move/LM-Photos/blob/main/MainPage%20Image/SubPhoto/5.png?raw=true",
-    path: "/",
     category1: "인테리어",
     category2: "소품",
     value1: "40%",
@@ -45,8 +41,15 @@ const PhotoList = [
 ];
 
 function MainPageList(props) {
+  // 애니메이션 테스트용 최종하기전 지워야함
   function changeTheImg(event) {
     console.log("가가각");
+  }
+
+  const navigate = useNavigate();
+
+  function handleCategoryClick(category) {
+    navigate(`list/${category}`);
   }
   return (
     <Container>
@@ -54,16 +57,25 @@ function MainPageList(props) {
         {PhotoList.map((photo, index) => {
           return (
             <Li key={photo.src + index}>
-              <a href={photo.path}>
+              <button
+                onClick={() => handleCategoryClick(photo.category1)} // category1 기반으로 이동
+                style={{
+                  background: "none",
+                  border: "none",
+                  padding: 0,
+                  cursor: "pointer",
+                }} // a 태그 스타일과 비슷하게 수정
+              >
                 <Img
                   src={photo.src}
                   alt={photo.category1}
                   onMouseOver={changeTheImg}
                   onMouseOut={changeTheImg}
                 />
+                {/* a에서 버튼으로 변경했으니 밑에 span 필요없을거같음 시간 나면 고치기*/}
                 <Span sizevalue={photo.value1}>{photo.category1}</Span>
                 <Span sizevalue={photo.value2}>{photo.category2}</Span>
-              </a>
+              </button>
             </Li>
           );
         })}
