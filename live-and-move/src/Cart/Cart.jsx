b0d6200694ef865d98b602db2c4cf61962d2843a
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-09-30 01:32:56 +0900

    fix:[Cart] fix layout

--- live-and-move/src/Cart/Cart.jsx
+++ live-and-move/src/Cart/Cart.jsx
@@ -21,57 +21,16 @@ import {
 } from "./CSS/Cart";
 import { isSessionExists } from "../Login/Account/AccountChk";
 
-const arrChoice = [
-  {
-    id: 1,
-    src: "https://www.ikea.com/kr/ko/images/products/poaeng-low-back-armchair-natural-colour-beige-katorp-natural-colour-beige__1315067_pe940386_s5.jpg?f=m",
-    name: "가구1",
-    content: "의자1",
-    price: 60000,
-    path: "/",
-  },
-  {
-    id: 2,
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR73qr3k_i1LIenLXL9iAogEQSP-L_YRljZug&s",
-    name: "가구2",
-    content: "의자2",
-    price: 50000,
-    path: "/",
-  },
-  {
-    id: 3,
-    src: "https://www.ikea.com/kr/ko/images/products/poaeng-low-back-armchair-natural-colour-beige-katorp-natural-colour-beige__1315067_pe940386_s5.jpg?f=m",
-    name: "가구3",
-    content: "의자3",
-    price: 40000,
-    path: "/",
-  },
-  {
-    id: 4,
-    src: "https://www.ikea.com/kr/ko/images/products/poaeng-low-back-armchair-natural-colour-beige-katorp-natural-colour-beige__1315067_pe940386_s5.jpg?f=m",
-    name: "가구4",
-    content: "의자4",
-    price: 40000,
-    path: "/",
-  },
-  {
-    id: 5,
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR73qr3k_i1LIenLXL9iAogEQSP-L_YRljZug&s",
-    name: "가구5",
-    content: "의자5",
-    price: 40000,
-    path: "/",
-  },
-];
-
 function Cart(props) {
+  const deliveryCost = useRef(10000);
+  const discountCost = useRef(0);
+
   const [cartData, setCartData] = useState({
     cartInfo: [],
     productInfo: [],
     itemNum: [],
   });
-  const [choice, setChoice] = useState(arrChoice);
-  // const [itemNum, setItemNum] = useState(choice.map(() => 1)); // 제품 수량
+
   const isLoadData = useRef(true);
   const URL = useRef("http://localhost:8080/api/cart/list");
 
@@ -97,7 +56,7 @@ function Cart(props) {
     await setCartData({
       cartInfo: [...data.data_cart],
       productInfo: [...data.data_img],
-      itemNum: [...data.data_img.map(() => 1)],
+      itemNum: [...data.data_cart.map(({ quantity }) => quantity)],
     });
 
     console.log("[ product data ]");
@@ -127,7 +86,13 @@ function Cart(props) {
   };
   //총 구독상품 금액
   const totalGD = cartData.itemNum.reduce((total, currentNum, index) => {
-    return total + currentNum * choice[index].price;
+    return (
+      total +
+      currentNum *
+        Math.ceil(
+          cartData.productInfo[index].price / cartData.cartInfo[index].month
+        )
+    );
   }, 0);
 
   // 체크박스 선택
@@ -144,7 +109,7 @@ function Cart(props) {
   const handleAllCheck = (checked) => {
     if (checked) {
       const idArray = [];
-      choice.forEach((el) => idArray.push(el.id));
+      cartData.cartInfo.forEach((el) => idArray.push(el.cart_id));
       setCheckItems(idArray);
     } else {
       setCheckItems([]);
@@ -152,29 +117,36 @@ function Cart(props) {
   };
   // 선택된 항목 삭제
   const handleDeleteSelected = () => {
-    const updatedChoice = choice.filter(
-      (item) => !checkItems.includes(item.id)
+    const updatedChoice = cartData.cartInfo.filter(
+      (item) => !checkItems.includes(item.cart_id)
     );
     const updatedItemNum = cartData.itemNum.filter(
-      (_, index) => !checkItems.includes(choice[index].id)
+      (_, index) => !checkItems.includes(cartData.cartInfo[index].cart_id)
     );
     setCheckItems([]);
-    setCartData({ ...cartData, itemNum: updatedItemNum });
-    setChoice(updatedChoice);
+    setCartData({
+      ...cartData,
+      cartInfo: updatedChoice,
+      itemNum: updatedItemNum,
+    });
+    // setChoice(updatedChoice);
   };
 
   // 개별 항목 삭제
   const handleDeleteItem = (index) => {
-    const updatedChoice = choice.filter((_, idx) => idx !== index);
+    const updatedChoice = cartData.cartInfo.filter((_, idx) => idx !== index);
     const updatedItemNum = cartData.itemNum.filter((_, idx) => idx !== index);
-    setChoice(updatedChoice);
-    setCartData({ ...cartData, itemNum: updatedItemNum });
+    setCartData({
+      ...cartData,
+      cartInfo: updatedChoice,
+      itemNum: updatedItemNum,
+    });
   };
 
   return (
     <>
       <Container>
-        <h2 style={{ margin: "3rem" }}> 장바구니 </h2>
+        <h1 style={{ fontWeight: "bold", margin: "3rem" }}> 장바구니 </h1>
         <Row>
           <Col md={8}>
             <label style={{ display: "flex" }}>
@@ -182,7 +154,9 @@ function Cart(props) {
                 type="checkbox"
                 label=""
                 onChange={(e) => handleAllCheck(e.target.checked)}
-                checked={checkItems.length === choice.length ? true : false}
+                checked={
+                  checkItems.length === cartData.cartInfo.length ? true : false
+                }
               />
               전체선택
             </label>
@@ -254,13 +228,71 @@ function Cart(props) {
             </ButtonDel>
           </Col>
           <ColJM md={3}>
-            <Container>
-              <h3>주문 예상 금액</h3>
-              <p>총 구독상품 금액 {totalGD} 원</p>
-              <p>총 할인 금액 {} 원</p>
-              <p>총 배송비 {} 원</p>
+            <Container style={{ width: "370px", fontSize: "18px" }}>
+              <h3 style={{ marginBottom: "20px" }}>주문 예상 금액</h3>
+              {[
+                {
+                  title: "총 구독상품 금액",
+                  value: totalGD,
+                },
+                {
+                  title: "총 할인 금액",
+                  value: discountCost.current,
+                },
+                {
+                  title: "총 배송비",
+                  value: deliveryCost.current,
+                },
+              ].map(({ title, value }, index) => {
+                return (
+                  <>
+                    <p
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        width: "330px",
+                      }}
+                    >
+                      <p style={{ width: "170px", marginRight: "70px" }}>
+                        {title}
+                      </p>
+                      <p
+                        style={{
+                          display: "flex",
+                          width: "100px",
+                          justifyContent: "end",
+                        }}
+                      >
+                        {value.toLocaleString("ko-KR")} 원
+                      </p>
+                    </p>
+                  </>
+                );
+              })}
               <hr />
-              <p>결제 금액: {totalGD.toLocaleString("ko-KR")} 원</p>
+              <p
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  fontWeight: "bold",
+                  fontSize: "28px",
+                }}
+              >
+                결제 금액:
+                <p
+                  style={{
+                    margin: "0px 5px 0px 70px",
+                    color: "lightskyblue",
+                  }}
+                >
+                  {(
+                    totalGD +
+                    deliveryCost.current -
+                    discountCost.current
+                  ).toLocaleString("ko-KR")}
+                </p>{" "}
+                원
+              </p>
               <ButtonJM variant="light">
                 <Div3>
                   상품 주문하기
