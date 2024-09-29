3c1c1b7081ce5dc078785ce68aa328e9af9bfcd5
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-09-30 02:40:50 +0900

    fix:[Cart] info & layout

--- live-and-move/src/Cart/CSS/Cart.js
+++ live-and-move/src/Cart/CSS/Cart.js
@@ -1,52 +1,57 @@
 import styled from "styled-components";
-import { Badge, Button, Col, Form, FormCheck } from "react-bootstrap";
-import { IoArrowForwardCircle, IoArrowForwardCircleOutline } from "react-icons/io5";
+import { Button, Col, FormCheck } from "react-bootstrap";
+import { IoArrowForwardCircle } from "react-icons/io5";
 
 //Cart page
-export const FormCheckStyled = styled(FormCheck)`
-
-
-`;
-export const Div1 = styled.div` // +,-버튼
+export const FormCheckStyled = styled(FormCheck)``;
+export const Div1 = styled.div`
+  // +,-버튼
   display: flex;
-  align-items:center ;
+  align-items: center;
   gap: 0px;
-  margin-bottom: 20px
+  margin-bottom: 20px;
 `;
-export const Div2 = styled.div`// 구독상품 리스트
+export const Div2 = styled.div`
+  // 구독상품 리스트
   display: flex;
-  justify-content:space-between;
+  justify-content: space-between;
   border-bottom: 1px solid #ccc;
-  align-items:center ;
+  align-items: center;
   padding: 1em;
   &:first-child {
     border-top: 1px solid #ccc; /* 첫 번째 블록에만 윗줄 보더 추가 */
   }
 `;
-export const Div3 = styled.div` //상품주문하기 버튼
+export const Div3 = styled.div`
+  //상품주문하기 버튼
   display: flex;
-  justify-content:space-between;
-  align-items:center ;
+  justify-content: space-between;
+  align-items: center;
   padding: 1em;
-  
 `;
-export const ColCard = styled(Col)` //구독상품 리스트 테두리 
+export const ColCard = styled(Col)`
+  //구독상품 리스트 테두리
   //border-top: 1px solid #878787;
-  padding:2em;
-  
+  padding: 2em;
 `;
-export const Button1 = styled.button`// +,-버튼
+export const Button1 = styled.button`
+  // +,-버튼
   font-size: 30px;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  align-items:center;
+  width: 60px;
+  height: 60px;
+  text-align: center;
   border: none;
   cursor: pointer;
-  background:none ;
+  background: none;
 `;
-export const Input1 = styled.input` // + 숫자 -
-  width: 60px;
+export const Input1 = styled.input`
+  // + 숫자 -
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center; */
+  margin-left: 15px;
+  align-items: center;
+  width: 40px;
   height: 40px;
   text-align: center;
   font-size: 20px;
@@ -54,40 +59,44 @@ export const Input1 = styled.input` // + 숫자 -
   color: #2020d9;
 `;
 
-export const ButtonDel = styled(Button)` // 선택상품삭제 버튼
+export const ButtonDel = styled(Button)`
+  // 선택상품삭제 버튼
   width: 10em;
   height: 3em;
-  color:white;
-  margin-bottom: 5em; 
-  background-color:#0C0F67;
-  border:1px solid #ccc;
+  color: white;
+  margin-bottom: 5em;
+  background-color: #0c0f67;
+  border: 1px solid #ccc;
   &:hover {
     transform: scale(1.04);
-    background-color:#B0B8E6;
-    }
-    &:active {
-      transform: scale(0.98);
+    background-color: #b0b8e6;
+  }
+  &:active {
+    transform: scale(0.98);
   }
 `;
-export const ButtonJM = styled(Button)` // 상품 주문하기 버튼
+export const ButtonJM = styled(Button)`
+  // 상품 주문하기 버튼
   width: 100%;
   height: 5em;
-  color:white;
-  margin-bottom: 5em; 
-  background-color:#0C0F67;
-  border:1px solid #ccc;
+  color: white;
+  margin-bottom: 5em;
+  background-color: #0c0f67;
+  border: 1px solid #ccc;
   &:hover {
     transform: scale(1.04);
-    background-color:#B0B8E6;
-    }
-    &:active {
-      transform: scale(0.98);
+    background-color: #b0b8e6;
+  }
+  &:active {
+    transform: scale(0.98);
   }
 `;
-export const ColJM = styled(Col)` // 주문 예상 금액
+export const ColJM = styled(Col)`
+  // 주문 예상 금액
   margin-left: 50px;
 `;
-export const JMContainer = styled.div` // 주문 예상 금액
+export const JMContainer = styled.div`
+  // 주문 예상 금액
   background: #ededed;
   padding: 2em;
   border-radius: 20px;
@@ -96,6 +105,6 @@ export const JMContainer = styled.div` // 주문 예상 금액
   max-height: 350px;
 `;
 
-export const Icon1 = styled(IoArrowForwardCircle)` 
+export const Icon1 = styled(IoArrowForwardCircle)`
   font-size: 2rem;
 `;
